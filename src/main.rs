c7cffeaf96696756845fa269492159f6633e9f82
Bill McCann <bill.mccann@gmail.com>
2024-11-09 11:34:58 -0500

    Simple error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let file_result = File::open("nonexistent.txt");
+
+    match file_result {
+        Ok(file) => println!("File opened successfully: {:?}", file),
+        Err(error) => println!("Failed to open file: {:?}", error),
+    }
 }
